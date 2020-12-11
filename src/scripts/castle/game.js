481d71c4b5be771f628f1d6410ea6ef33463244a
jasminelok <jasminellok@gmail.com>
2020-12-11 18:05:23 -0500

    Fix prince room open

--- src/scripts/castle/game.js
+++ src/scripts/castle/game.js
@@ -5,7 +5,6 @@ export default class Game {
     constructor() {
         this.board = new Board(3, 5);
         this.guardLocation = this.board.roomMap.guard;
-        //starting new game will initialize stuff
 
         this.newGame = this.newGame.bind(this);
         this.openRoom = this.openRoom.bind(this);
@@ -21,7 +20,7 @@ export default class Game {
         this.lifeCount = 2
         this.guardLocation = this.board.roomMap.guard;
         this.shownRooms = [];
-        this.dodgeHits = null; //minigame
+        this.dodgeHits = null;
     }
 
     openRoom(room) {
@@ -31,16 +30,19 @@ export default class Game {
         this.shownRooms.push([r,c])
         suit.drawSuit();
         suit.displayMessage();
+        let guardI=-1;
+        this.guardLocation.forEach((ele, i) => {
+            if ((ele[0] === r) && (ele[1]===c)) {
+                guardI = i;
+            }
+        });
+        if (guardI>=0) {
+            this.guardLocation.splice(guardI,1);
+            console.log('after splice',this.guardLocation)
+        }
+        console.log("openroom", this.guardLocation)
         if (suit.value === 'guard' || suit.value === 'commander' || 
             suit.value === 'storage' || suit.value === 'crown' || suit.value === 'prince') {
-                let guardI;
-                this.guardLocation.forEach((ele, i) => {
-                    if ((ele[0] === r) && (ele[1]===c)) {
-                        guardI = i;
-                    }
-                });
-                if (guardI) this.guardLocation.splice(guardI,1);
-                console.log("openroom", this.guardLocation)
                 return suit.value
         } else {
             return false
@@ -65,16 +67,14 @@ export default class Game {
                 this.gameWin()
                 break
             case 'prince':
-                if (this.guardLocation.length > 1) {
-                    // debugger;
-                    const roomNum = this.guardLocation[1];
+                if (this.guardLocation.length > 0) {
+                    const roomNum = this.guardLocation[0];
                     const [r, c] = [`${roomNum[0]}`, `${roomNum[1]}`];
-                    const roomVal = document.getElementById(`${r}-${c}`);
+                    const roomVal = document.getElementsByClassName(`suit-value-[${r},${c}]`)[0];
                     const room = roomVal.parentElement.parentElement;
                     room.setAttribute('id','shown')
                     this.board.grid[roomNum[0]][roomNum[1]].drawSuit();
-                    this.board.grid[roomNum[0]][roomNum[1]].displayMessage();
-                    // debugger;
+                    roomVal.innerHTML = 'The prince revealed the location of a guard'
                 }
                 break
         }
