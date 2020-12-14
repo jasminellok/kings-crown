3f30fdf5e4e4284f1eb4f606390871463616b497
jasminelok <jasminellok@gmail.com>
2020-12-13 23:19:20 -0500

    Polish

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import "./styles/index.scss";
 import Game from "./scripts/castle/game.js"
 
 const playGame = () => {
-    let overlayMessages = Array.from(document.getElementsByClassName("overlay"));
-    let rooms =  Array.from(document.getElementsByClassName("room"));
+    let coverMessages = Array.from(document.getElementsByClassName("cover"));
+    let rooms = Array.from(document.getElementsByClassName("room"));
     let game;
+
+    coverMessages.forEach(msg => {
+        msg.addEventListener('click', () => {
+            rooms.forEach(room => {
+                room.setAttribute('id','hidden')
+            })
+            game = new Game();
+            game.newGame();
+            msg.classList.remove('shown');
+        })
+    })
 
     document.addEventListener("life-update", ()=> {
         document.getElementsByClassName('life-count')[0].innerHTML = game.lifeCount;
@@ -16,17 +27,6 @@ const playGame = () => {
         const c = game.currRoom.c;
         let msg = document.getElementsByClassName(`suit-value-[${r},${c}]`)[0];
         msg.innerHTML = "The commander should be always near the king... the crown should be in an adjacent room"
-    })
-
-    overlayMessages.forEach(msg => {
-        msg.addEventListener('click', () => {
-            rooms.forEach(room => {
-                room.setAttribute('id','hidden')
-            })
-            game = new Game();
-            game.newGame();
-            msg.classList.remove('shown');
-        })
     })
 
     rooms.forEach(room => {
@@ -43,8 +43,4 @@ const playGame = () => {
 
 }
 
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', playGame());
-} else {
-    playGame();
-}
+document.addEventListener('DOMContentLoaded', playGame());
